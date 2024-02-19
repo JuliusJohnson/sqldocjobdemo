3629ceab3fb35ad6bb2c2c92552e9011c8087f0b
Julius <contact@juliusj.com>
2024-02-18 23:47:16 -0500

    first commit

---
+++ test.py
@@ -0,0 +1,54 @@
+import csv
+from flask import Flask, render_template 
+
+# declaring app name 
+app = Flask(__name__) 
+
+def read_data():
+    with open('testjobdata.csv', mode = 'r') as jobsData:
+        dictJobsData = list(csv.DictReader(jobsData))
+        return dictJobsData
+    #listofDict = list(dict_reader)
+
+def get_jobnames():
+    jobNames = []
+    jobsData = read_data()
+    for job in jobsData:
+        jobNames.append(job['JobName'])   
+    uniqueJobNames = list(dict.fromkeys(jobNames))
+    return uniqueJobNames
+
+def select_jobsteps(job):
+    jobSteps = []
+    for i in read_data():
+        if i['JobName'] == str(job):
+            jobSteps.append(i)
+    return jobSteps
+
+# defining home page 
+@app.route('/') 
+def homepage(): 
+  
+# returning index.html and list 
+# and length of list to html page 
+    return render_template("index.html", len = len(select_jobsteps('Test1')), jobSteps = select_jobsteps('Test1')) 
+  
+    # if __name__ == '__main__': 
+  
+    # running app 
+    app.run(use_reloader = True, debug = True) 
+
+print(select_jobsteps('Test1'))
+
+
+
+# with open('testjobdata.csv', mode = 'r') as file:
+#     dict_reader = csv.DictReader(file)
+    
+#     listofDict = list(dict_reader)
+
+# for i in listofDict:
+#     if i['JobName'] == 'Test1':
+#         print(i)
+    
+# # print(listofDict[0]['JobName'])
