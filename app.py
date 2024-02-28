10c708589d14321f3d15ee7071821bd92915360c
Julius <contact@juliusj.com>
2024-02-27 22:27:00 -0500

    Updated to use an actuall sql server database

--- app.py
+++ app.py
@@ -1,38 +1,56 @@
-import csv
-from flask import Flask, render_template 
-
+import csv,dbConnection
+from flask import Flask, render_template, render_template_string, abort
+from pprint import pprint
 # declaring app name 
 app = Flask(__name__) 
 
-def read_data():
-    with open('testjobdata.csv', mode = 'r') as jobsData:
-        dictJobsData = list(csv.DictReader(jobsData))
-        return dictJobsData
-    #listofDict = list(dict_reader)
+def getJobStepData(jobName):
+    getJobStepData = dbConnection.getJobs(jobName)
+    return getJobStepData
 
-def get_jobnames():
-    jobNames = []
-    jobsData = read_data()
-    for job in jobsData:
-        jobNames.append(job['JobName'])   
-    uniqueJobNames = list(dict.fromkeys(jobNames))
-    return uniqueJobNames
+def structureData():
+    keys = []
+    columns = []
+    jobs = {}
 
-def select_jobsteps(job):
-    jobSteps = []
-    for i in read_data():
-        if i['JobName'] == str(job):
-            jobSteps.append(i)
-    return jobSteps
+    for job in getJobStepData(''):
+        keys.append(job[0])
+
+    for column in getJobStepData('').description:
+        columns.append(column[0])
+
+    keys = list(set((keys))) #makes unique list
+
+    for key in keys: #for each key in the keys list
+        jobs[key] = [] #create a dictionary entry with an empty list
+        for i in getJobStepData('').fetchall(): #for each "row" from the query while looping through keys
+            if i[0] == key: #if the jobname is equal to the key name
+                jobs[key].append(dict(zip(columns,i))) #append that row to the corresponding key name. Help: https://stackoverflow.com/questions/16519385/output-pyodbc-cursor-results-as-python-dictionary
+        
+    return jobs
+
+pprint(structureData()['Long Job'][0]['step_name'])
 
 # defining home page 
-@app.route('/') 
+@app.route(f'/') 
 def homepage(): 
   
 # returning index.html and list 
 # and length of list to html page 
-    return render_template("index.html", len = len(select_jobsteps('Test1')), jobSteps = select_jobsteps('Test1')) 
-  
+    return render_template("index.html", len = len(structureData()['Long Job']), jobSteps = structureData()['Long Job']) 
+
+# @app.route('/<string:title>')
+# def post(number):
+#     if number < 1 or number > len(select_jobsteps('t')):
+#         abort(404, "This Post doesn't exist")   
+
+#     data = select_jobsteps('t')[number-1]
+    
+#     return render_template_string('''
+# {{ item[0]['JobName'] }}<br/>
+# {{ item.content }}<br/>
+# {{ item.date }}<br/>
+# {{ item.author }}<br/>''', item=select_jobsteps('t')) 
 # if __name__ == '__main__': 
   
 # running app 
---
+++ dbConnection.py
@@ -0,0 +1,60 @@
+import pyodbc
+SERVER = "Julius"
+DATABASE= "Test"
+
+
+def getJobs(jobName):
+    conn = pyodbc.connect(f'DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={SERVER};DATABASE={DATABASE};Trusted_Connection=Yes;Encrypt=No;APP=SQL Server Job Report;')
+    #conn = pyodbc.connect('Driver={ODBC Driver 18 for SQL Server};Server=test;UID=user;PWD=password;Encrypt=no;')
+
+    cursor = conn.cursor()
+    ### SQL QUERY START ###
+    jobStepsSql= f'''  DECLARE @jobname varchar(100) 
+
+  SET @jobname = {jobName}
+
+  IF @jobname = ''
+	BEGIN
+      SET  @jobname = '%';
+	END;
+    ELSE
+        SET @jobname = @jobname;
+    -- Insert statements for procedure here
+    SELECT sj.name AS JobName,
+           sj.enabled,
+           sj.start_step_id,
+           sjs.step_id,
+           sjs.step_name,
+           sjs.subsystem,
+           sjs.command,
+           CASE on_success_action
+               WHEN 1 THEN
+                   'Quit with success'
+               WHEN 2 THEN
+                   'Quit with failure'
+               WHEN 3 THEN
+                   'Go to next step'
+               WHEN 4 THEN
+                   'Go to step ' + CAST(on_success_step_id AS VARCHAR(3))
+           END On_Success,
+           CASE on_fail_action
+               WHEN 1 THEN
+                   'Quit with success'
+               WHEN 2 THEN
+                   'Quit with failure'
+               WHEN 3 THEN
+                   'Go to next step'
+               WHEN 4 THEN
+                   'Go to step ' + CAST(on_fail_step_id AS VARCHAR(3))
+           END On_Failure,
+           sj.delete_level,
+           last_run_date,
+           last_run_duration,
+           last_run_outcome
+    FROM msdb.dbo.sysjobs AS sj
+        INNER JOIN msdb.dbo.sysjobsteps sjs
+            ON sj.job_id = sjs.job_id
+    WHERE sj.name LIKE @JobName'''
+    ### SQL QUERY END ###
+    cursor.execute('EXEC Test.dbo.proc_JobSteps @jobname = ?', jobName)
+    return cursor
